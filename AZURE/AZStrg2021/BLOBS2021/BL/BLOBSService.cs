f43961b8d27b15da05e5ef321048596a9d62643d
nbende <nbende@gmail.com>
2021-09-01 12:56:00 +0530

    AZ storage

---
+++ AZURE/AZStrg2021/BLOBS2021/BL/BLOBSService.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Storage.Blobs; //
+using Azure.Storage.Blobs.Models; //
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLOBS2021.BL
+{
+    public class BLOBSService
+    {
+
+        private readonly BlobServiceClient _blobClient;
+
+        public BLOBSService(BlobServiceClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+        public async Task<IEnumerable<string>> GetBlobs(string containerName)
+        {
+            // allow us to access the data inside the container
+            var containerClient = _blobClient.GetBlobContainerClient(containerName);
+
+            var files = new List<string>();
+
+            var blobs = containerClient.GetBlobsAsync();
+
+            await foreach (var item in blobs)
+            {
+                var blobClient = containerClient.GetBlobClient(item.Name);
+                files.Add(blobClient.Uri.AbsoluteUri);
+            }
+
+            return files;
+        }
+
+        public async Task<bool> UploadBlob(string name, IFormFile file, string containerName)
+        {
+            var containerClient = _blobClient.GetBlobContainerClient(containerName);
+
+            // checking if the file exist 
+            // if the file exist it will be replaced
+            // if it doesn't exist it will create a temp space until its uploaded
+            var blobClient = containerClient.GetBlobClient(name);
+
+            var httpHeaders = new BlobHttpHeaders()
+            {
+                ContentType = file.ContentType
+            };
+
+            var res = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+
+            if (res != null)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ AZURE/AZStrg2021/BLOBS2021/Controllers/BLOBController.cs
@@ -0,0 +1,42 @@
+﻿using BLOBS2021.BL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLOBS2021.Controllers
+{
+    public class BLOBController : Controller
+    {
+        BLOBSService _blobservice;
+        public BLOBController(BLOBSService blobsservice)
+        {
+            _blobservice = blobsservice;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var files = await _blobservice.GetBlobs("images");
+            return View(files);
+        }
+
+        public async Task<IActionResult> Add(IFormFile file)
+        {
+            if (file == null || file.Length < 1) return View();
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+            var res = await _blobservice.UploadBlob(fileName, file, "images");
+
+            if (res)
+                return RedirectToAction("Index");
+
+            return View();
+
+            
+        }
+
+    }
+}
