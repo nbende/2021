fb5bd81d182ee4c22921ff5fe63ecf8135b599d7
nbende <nbende@gmail.com>
2021-09-17 00:00:02 +0530

    added ocelot demo for microservices & AZURE SQL AZURE demo with Code First

---
+++ AZURE/AZSQLCF/Data/PrjContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AZSQLCF.Models; //
+using Microsoft.EntityFrameworkCore; //
+
+namespace AZSQLCF.Data
+{
+    public class PrjContext : DbContext
+    {
+        public PrjContext(DbContextOptions<PrjContext> options) : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
